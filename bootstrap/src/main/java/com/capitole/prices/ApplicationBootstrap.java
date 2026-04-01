2187a78a4130a314989e6c2238cb9764445176b3
John Quiroga <johnquirogac@gmail.com>
2026-04-01 11:53:53 -0500

    Hexagonal arquitecture first setup with bootstrap, adapter, application and domain modules

---
+++ bootstrap/src/main/java/com/capitole/prices/ApplicationBootstrap.java
@@ -0,0 +1,13 @@
+package com.capitole.prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBootstrap.class, args);
+    }
+
+}
